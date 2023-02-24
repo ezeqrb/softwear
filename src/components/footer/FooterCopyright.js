f83feba8297644fb58d9d0ac7c9fe6344f38fa8e
ezeqrb <ezeqrb19@gmail.com>
2023-02-24 11:12:32 -0300

    first commit

---
+++ src/components/footer/FooterCopyright.js
@@ -0,0 +1,38 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const FooterCopyright = ({ footerLogo, spaceBottomClass, colorClass }) => {
+  return (
+    <div
+      className={`copyright ${spaceBottomClass ? spaceBottomClass : ""} ${
+        colorClass ? colorClass : ""
+      }`}
+    >
+      <div className="footer-logo">
+        <Link to={process.env.PUBLIC_URL + "/"}>
+          <img alt="" src={process.env.PUBLIC_URL + footerLogo} />
+        </Link>
+      </div>
+      <p>
+        &copy; {new Date().getFullYear()}{" "}
+        <a
+          href="https://hasthemes.com"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Flone
+        </a>
+        .<br /> All Rights Reserved
+      </p>
+    </div>
+  );
+};
+
+FooterCopyright.propTypes = {
+  footerLogo: PropTypes.string,
+  spaceBottomClass: PropTypes.string,
+  colorClass: PropTypes.string
+};
+
+export default FooterCopyright;
